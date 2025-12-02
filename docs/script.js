7e7e091936d27c6fc8fc2e7d9e30058f2f557955
JosefaChacana <josefa.chacana@uc.cl>
2025-12-02 17:16:10 -0300

    .

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
-const NUM_SPARKLES = 2000; 
+
+const NUM_SPARKLES = 50; 
 
 const introContainer = document.getElementById('intro-animation'); 
 
@@ -7,18 +8,17 @@ if (introContainer) {
         const sparkle = document.createElement('div');
         sparkle.classList.add('sparkle');
 
-    
+
         sparkle.style.left = Math.random() * 100 + 'vw';
         sparkle.style.top = Math.random() * 100 + 'vh';
         
       
-        const duration = Math.random() * 3 + 1;
+        const duration = Math.random() * 3 + 1; 
         const delay = Math.random() * 4; 
 
         sparkle.style.animationDuration = duration + 's';
         sparkle.style.animationDelay = delay + 's';
 
-       
         introContainer.appendChild(sparkle); 
     }
 }
